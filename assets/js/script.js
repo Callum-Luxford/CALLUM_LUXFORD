cc4c62211966abe3589ab48b81d5941243e3363d
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2024-08-28 18:04:25 +0100

    Several additions:
    Building out the main cards and sections.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,73 @@
+// Mouse circle
 $(document).mousemove(function (e) {
-  $('.circle').css({
+  $(".circle").css({
     left: e.pageX - 125,
     top: e.pageY - 125,
   });
 });
+// End Mouse circle
+
+// Type Writer
+var typed = new Typed(".auto-type", {
+  strings: [
+    "",
+    "",
+    "Hello user.. ^2000",
+    "I'm Callum.. ^2000",
+    "Welcome to my Portfolio :) ",
+  ],
+  typeSpeed: 50,
+  backSpeed: 75,
+  loop: false,
+});
+// End Type Writer
+
+// Scroll reveal
+const scrollRevealOption = {
+  distance: "25px",
+  origin: "right",
+  duration: 500,
+};
+ScrollReveal().reveal(".main__content", {
+  ...scrollRevealOption,
+  delay: 250,
+});
+ScrollReveal().reveal(".header__content", {
+  ...scrollRevealOption,
+  delay: 250,
+  origin: "left",
+});
+ScrollReveal().reveal(".header__icons", {
+  ...scrollRevealOption,
+  delay: 250,
+  origin: "bottom",
+});
+// End Scroll reveal
+
+
+// 
+const aboutInfo = document.getElementById("about__info");
+
+// about.addEventListener("click", (e) => {
+//   aboutInfo.style.visibility = "visible";
+//   ScrollReveal().reveal(aboutInfo,{
+//     ...scrollRevealOption,
+//     delay: 100,
+//     origin: "top",
+//   });
+// });
+
+// function toggleNav(){
+//   let x = aboutInfo;
+//   if(x.style.visibility === "hidden"){
+//     x.style.visibility = "visible";
+//   } else {
+//     x.style.visibility = "hidden";
+//   }
+// };
+
+// $('#about').click(function(){
+//   $('.about__info').slideToggle(500, "linear");
+//   aboutInfo.style.display = "grid";
+// })
+
