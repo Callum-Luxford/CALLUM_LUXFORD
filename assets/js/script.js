1fa1dc58a8ed1de27271f7fdbaad3ccccc76eb70
Callum Luxford <151152271+Callum-Luxford@users.noreply.github.com>
2024-09-24 17:24:00 +0100

    Changes to script, css, html.
    Link to project additions.

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,9 +63,30 @@ cards.forEach(function (card) {
 
 // Card hover plane
 const pCards = document.querySelectorAll(".project__card");
-const plane = document.querySelectorAll(".plane")
+const plane = document.querySelectorAll(".plane");
+// End Card hover plane
 
-
+// Card project links
+let project1 = document.getElementById("project__1");
+project1.addEventListener("click", () => {
+  window.open("https://dynamicroofingsolutionsltd.co.uk/", "_blank");
+});
+let project2 = document.getElementById("project__2");
+project2.addEventListener("click", () => {
+  window.open("https://github.com/Callum-Luxford/PYTHON-PONG", "_blank");
+});
+let project3 = document.getElementById("project__3");
+project3.addEventListener("click", () => {
+  window.open("https://github.com/Callum-Luxford/PYTHON-SNAKE", "_blank");
+});
+let project4 = document.getElementById("project__4");
+project4.addEventListener("click", () => {
+  window.open(
+    "https://github.com/Callum-Luxford/PYTHON-POMODORO/tree/main",
+    "_blank"
+  );
+});
+// End Card project links
 
 // Type Writer
 var typed = new Typed(".auto-type", {
